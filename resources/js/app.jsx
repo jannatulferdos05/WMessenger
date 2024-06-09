6cd844b4ba8aa1bb0653fbf9c72ed3f1e76b3b09
jannatulferdos05 <jannatulferdos587@gmail.com>
2024-06-10 02:03:39 +0600

    backendConnection

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,7 @@
 import React from "react" ;
 import ReactDOM from "react-dom/client";
-import Message from "./components/Message";
+import App from './components/App';
 
-    
 
 let root=ReactDOM.createRoot(document.getElementById("app"));
-root.render(<Message/>);
+root.render(<App />);
--- resources/js/components/Message.jsx
+++ resources/js/components/Message.jsx
@@ -5,20 +5,28 @@ import '../../css/message.css';
 function Message() {
 
 
-    const [phone, setPhone] = useState(
-        {
-            phone: "phone"
-        }
-    );
+    const [phone, setPhone] = useState("");
 
 
-    const [message, setMessage] = useState({
-        message: "message"
+    const [message, setMessage] = useState("");
     
-    });
-    
+
+    function handlePhone(event) {
+        setPhone(event.target.value);
+    }
+
+    function handleMessage(event) {
+        setMessage(event.target.value);
+    }
+
     function handleSubmit(event) {
-        return null;
+        const data={phone,message};
+        fetch("http://bulksmsbd.net/api/smsapi?api_key=cQcIRLoSoBKMBwL9jIVR&type=text&number=8801516009099&senderid=8809617614158&message=TestSMS ",{
+            method: "POST",
+            body:JOSN.stringify(data)
+        }).then(response=>response.json())
+        .then(data=>{console.log(data); })
+        .catch((error)=>{console.error(error)});
     }
 
 
@@ -26,13 +34,13 @@ function Message() {
 return (
 <div>
 <h1 className="head" >Send Messages</h1>
-<form onChange={handleSubmit} method="post">
+<form onSubmit={handleSubmit} >
 
 <label>Phone: <br/>
-<input type="text" className="phonenumber" name="phone" placeholder="Enter receiver phone number"/><br/>
+<input value={phone}  onChange={handlePhone} type="text" className="phonenumber" name="phone" placeholder="Enter receiver phone number"/><br/>
 </label><br/>
 <label>Message:<br/>
-<input type="text" name="message" placeholder="Enter your message"/><br/>
+<input value={message} onChange={handleMessage} type="text" name="message" placeholder="Enter your message"/><br/>
 </label>
 <br/>
 <button type="submit" >Send</button>
