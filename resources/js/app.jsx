800485d760a9d45e048398bdfb5d9ccb9fbf4408
Sharif Hasan <me.sharif.hasan@gmail.com>
2024-06-10 02:07:14 +0600

    my changes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,8 @@
 import React from "react" ;
 import ReactDOM from "react-dom/client";
 import Message from "./components/Message";
-
+import App from "./components/App";
     
 
 let root=ReactDOM.createRoot(document.getElementById("app"));
-root.render(<Message/>);
+root.render(<App/>);
